6e6881bf4365269fc07a1b282f5d3c48db8a7675
CesarPiresSevero <cesar.pires.severo@gmail.com>
2025-02-17 20:09:23 +0100

    Complete header file with doxygen

--- src/tinynoise.h
+++ src/tinynoise.h
@@ -1,25 +1,65 @@
-/*
- * ============================================================================
- *
- *       Filename:  tinynoise.h
- *
- *    Description:  TinyNoise header
- *
- *        Created:  02/16/2025
- *
- *         Author:  Cesar Pires Severo
- *
- * ============================================================================
- */
+/**
+* @file tinynoise.h
+* @brief TinyNoise library header file
+* @author Cesar Pires Severo
+* @date 2/17/2025
+*/
 
 #ifndef TINYNOISE_H
 #define TINYNOISE_H
 
 #include <stdint.h>
 
-void tn_get_config();
-void tn_set_config();
-uint16_t tn_run();
+/**
+ * @brief Signal distribution type 
+ */
+typedef enum
+{
+    NORMAL,
+    UNIFORM
+}distrib_t;
+
+/**
+ * @brief Noise color type
+ */
+typedef enum
+{
+    WHITE,
+    BROWN,
+    PINK
+}color_t;
+
+/**
+* @brief Distribution type set function
+* @param new_distrib Distribution type (NORMAL or UNIFORM)
+*/
+void tn_set_distrib(distrib_t new_distrib);
+
+/**
+* @brief Noise color set function
+* @param new_color Noise color to be used (WHITE, BROWN or PINK)
+*/
+void tn_set_color(color_t new_color);
+
+/**
+* @brief Starter seeds set function
+* @param seed1 First start seed (unsigned integer 16bits)
+* @param seed2 Second start seed (unsigned integer 16bits)
+* @param seed3 Third start seed (unsigned integer 16bits)
+*/
+void tn_set_seed(uint16_t seed1, uint16_t seed2, uint16_t seed3);
+
+/**
+* @brief Resets all library parameters to default
+*/
+void tn_reset(void);
+
+/**
+* @brief Noise generation function  
+* @return uint16_t New noise data point
+*/
+uint16_t tn_run(void);
 
 
 #endif // TINYNOISE_H
+
