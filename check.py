0a98ebfe0e44ee31af4fe4221145ca3633408be7
CesarPiresSevero <cesar.pires.severo@gmail.com>
2025-03-01 11:59:21 +0100

    Added filters for testing

--- check.py
+++ check.py
@@ -1,9 +1,11 @@
+# Imports
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy
 
-plot_title = "Numpy Random Uniform"
+plot_title = "Tiny Noise Uniform High Pass"
 
+# Helper Funcs
 def get_fft(array,fs):
     ts=1/(int(fs))
     t=np.arange(0,(len(array)*ts),ts)
@@ -11,21 +13,23 @@ def get_fft(array,fs):
     fr=(int(fs)/2)*np.linspace(0,1,int(n/2))
     resp=np.fft.fft(array)
     freq=(2/n)*abs(resp[0:np.size(fr)]) 
-    #(2/n) is for normalizing since the FFT size is set as the size of the input array 
-    # FFT amplitude is always half of the FFT points
-    # In this case N-FFT ( np.fft.fft(array,len(array)) ) is the same as the lenght of the input array ( len(array) )
-    return freq,fr,t #Array, frequency in hz, time in sec
+    return freq,fr,t 
 
-# f = open("log1.txt", "r")
-# lines = f.readlines()
-# data =[]
-# for line in lines:
-#     line = line.replace("\n", "")
-#     line=int(line,16)
-#     line=(line/(2**15-1))-1
-#     data.append(line)
-# output=np.asarray(data, dtype=np.float32)
+def lp_filter(indata):
+    data = [0]
+    for i,val in enumerate(indata):
+        data.append((val+data[-1])/2)
+    return np.asarray(data)
 
+
+def hp_filter(indata):
+    data = [0]
+    for i,val in enumerate(indata):
+        data.append(val-(val+data[-1])/2)
+    return np.asarray(data)
+
+
+# Reading log file from C code
 f = open("log.txt", "r")
 lines = f.readlines()
 data =[]
@@ -36,11 +40,18 @@ for line in lines:
     data.append(line)
 output=np.asarray(data, dtype=np.float32)
 
-output=np.random.normal(scale=0.25,size=10**5)
-output=np.random.uniform(low=-1.0,high=1.0,size=10**5)
+# Numpy random generators
+# output=np.random.normal(scale=0.25,size=10**5)
+# output=np.random.uniform(low=-1.0,high=1.0,size=10**5)
 
+# Filtering data
+# output = lp_filter(output)
+# output = lp_filter(lp_filter(output))
+# output = hp_filter(output)
+output = hp_filter(hp_filter(output))
+
+# Plotting data
 output_fft,output_fr,output_t=get_fft(output,1)
-
 data=np.asarray(output, dtype=np.float32)
 fig1, (ax1_1, ax2_1, ax3_1) = plt.subplots(1, 3)
 fig1.suptitle(plot_title)
